a8365f7d7f61d091c2f7724cee0639d070584b63
kishore-seemala <kishore.seemala@gmail.com>
2012-07-10 10:48:29 -0300

    Update master

--- square.rb
+++ square.rb
@@ -2,9 +2,9 @@ class Square
 
    def square(a)
 
-   b=a*a             #b is a local variable declared and a is parameter passed
-   print "\n_________ to print the square of a given number ___________\n\n"
-   print "the square of #{a} is : "+b.to_s+"\n\n"
+      b=a*a             #b is a local variable declared and a is parameter passed
+      print "\n_________ to print the square of a given number ___________\n\n"
+      print "the square of #{a} is : "+b.to_s+"\n\n"
 
    end
 
